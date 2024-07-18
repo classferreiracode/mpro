46f347ec2acc96a7062b45c94c21d0cbb6c7cac7
Wagner Ferreira <66226535+classferreiracode@users.noreply.github.com>
2024-07-17 22:13:23 -0300

    install and config vue/sweetAlert2/tailwind/breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,36 @@
-import './bootstrap';
+import "./bootstrap";
+
+import Alpine from "alpinejs";
+import { createApp } from "vue";
+import Swal from "sweetalert2";
+
+window.Swal = Swal;
+
+const toastMixin = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 4000,
+    timerProgressBar: true,
+});
+
+window.Toast = toastMixin;
+
+const app = createApp({});
+Object.entries(import.meta.glob("./**/*.vue", { eager: true })).forEach(
+    ([path, definition]) => {
+        app.component(
+            path
+                .split("/")
+                .pop()
+                .replace(/\.\w+$/, ""),
+            definition.default
+        );
+    }
+);
+
+app.mount("#app");
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ resources/js/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Test</h1>
+        <button class="rounded px-4 py-2 text-red-500" @click="showAlert">Hello
+            world</button>
+    </div>
+</template>
+<script setup>
+const showAlert = () => {
+
+    Toast.fire(
+        '',
+        'Hello Vue world!!!',
+        'success'
+    );
+}
+</script>
